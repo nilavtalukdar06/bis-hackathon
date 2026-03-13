be1a93cd90631e3de172af5dcf0eeb7fc606d281
Web <webd75369@gmail.com>
2026-03-13 10:33:32 +0530

    updated env config

--- src/utils/config/env.ts
+++ src/utils/config/env.ts
@@ -2,11 +2,16 @@ import { z } from "zod";
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url({ message: "database url is not valid" }),
-  UPSTASH_REDIS_REST_URL: z
-    .string()
-    .url({ message: "redis url is not valid" }),
+  UPSTASH_REDIS_REST_URL: z.string().url({ message: "redis url is not valid" }),
   UPSTASH_REDIS_REST_TOKEN: z
-    .string().min(1, { message: "redis token is required" }),
+    .string()
+    .min(1, { message: "redis token is required" }),
+  GOOGLE_CLIENT_ID: z
+    .string()
+    .min(1, { message: "google client id is required" }),
+  GOOGLE_CLIENT_SECRET: z
+    .string()
+    .min(1, { message: "google client secret is required" }),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
